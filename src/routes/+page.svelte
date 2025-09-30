55a5280a88570495a020411ae831df7f77270272
clintt-09 <aisosaclinton09@gmail.com>
2025-09-30 13:55:45 +0100

    feat: initialize SvelteKit project with Tailwind CSS and basic structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let count = $state(1)
+
+    function addCount(){
+        count ++;
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button onclick={addCount}>
+    click me
+</button>
+<h2>{count}</h2>
