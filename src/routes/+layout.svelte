568fef6446615e50d88a2011a11b1d26b8836f0d
clintt-09 <aisosaclinton09@gmail.com>
2025-10-01 13:27:38 +0100

    feat: enhance SEO with OpenGraph and Twitter Card meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,30 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Svelte+Convex Chat</title>
+	<meta
+		name="description"
+		content="chat live with anyone, no sign ups. built with svelte and convex"
+	/>
+
+	<!-- OpenGraph -->
+	<meta property="og:title" content="Svelte+Convex Chat" />
+	<meta
+		property="og:description"
+		content="chat live with anyone, no sign ups. built with svelte and convex."
+	/>
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://svelte-chat-app.pages.dev/" />
+	<meta property="og:image" content={favicon} />
+
+	<!-- Twitter Card -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content="Svelte+Convex Chat" />
+	<meta
+		name="twitter:description"
+		content="chat live with anyone, no sign ups. built with svelte and convex"
+	/>
+	<meta name="twitter:image" content={favicon} />
 </svelte:head>
 
 {@render children?.()}
